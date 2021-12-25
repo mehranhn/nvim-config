7943453c1f90c7b1428edf958f3aa88fcfa2e894
MHN <mehranhhm@gmail.com>
2021-12-25 17:12:36 +0330

    added toggle term

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -194,17 +194,17 @@ local mappings = {
     w = { "<CMD>lua require('spectre').open_visual({select_word=true})<CR>", "Replace Workspace (Word)"},
   },
 
-  -- t = {
-  --   name = "Terminal",
-  --   g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
-  --   n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-  --   d = { "<cmd>lua _LAZYDOCKER_TOGGLE()<cr>", "NCDU" },
-  --   t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
-  --   p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-  --   f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-  --   h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-  --   v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-  -- },
+  t = {
+    name = "Terminal",
+    g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+    -- n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+    d = { "<cmd>lua _LAZYDOCKER_TOGGLE()<cr>", "Lazydocker" },
+    t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+    -- p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+    x = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+  },
 }
 
 which_key.setup(setup)
