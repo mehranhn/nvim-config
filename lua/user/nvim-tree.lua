ae69758ae5e03f3af16b8abf1c5cc9f3c2b9b763
MHN <mehranhhm@gmail.com>
2022-01-13 21:22:48 +0330

    removed an extra space

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -96,7 +96,7 @@ nvim_tree.setup {
         { key = "P",                            cb = tree_cb("parent_node") },
         { key = "<BS>",                         cb = tree_cb("close_node") },
         { key = "<S-CR>",                       cb = tree_cb("close_node") },
-        { key = "<S-Tab>",                        cb = tree_cb("preview") },
+        { key = "<S-Tab>",                      cb = tree_cb("preview") },
         { key = "K",                            cb = tree_cb("first_sibling") },
         { key = "J",                            cb = tree_cb("last_sibling") },
         { key = "I",                            cb = tree_cb("toggle_ignored") },
