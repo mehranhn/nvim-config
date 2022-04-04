cb9ed60b1c0ef4cb5fa6e6f46f4c9197ae3e21db
MHN <mehranhhm@gmail.com>
2022-04-04 22:08:51 +0430

    removed extra comments

--- init.lua
+++ init.lua
@@ -1,8 +1,1 @@
 require("user")
-
--- vim.ui.select({'a', 'b'}, { prompt = 'Choose Canister' }, function(selection)
---     if selection == nil then
---         print('nil')
---     end
---     print(selection)
--- end)
