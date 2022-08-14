38a9f3c4ad74f18403d3c2842974d435f09b9115
Mehran <mehranhhm@gmail.com>
2022-08-15 00:24:59 +0430

    config update

--- lua/user/lsp/servers/sqls.lua
+++ lua/user/lsp/servers/sqls.lua
@@ -3,4 +3,12 @@ require('lspconfig').sqls.setup{
     cmd = {"sqls"}, -- , "-config", "path/to/config.yml"};
     on_attach = require("user.lsp.handlers").on_attach,
     capabilities = capabilities,
+    settings = {
+        sqls = {
+            {
+                driver = 'postgresql',
+                dataSourceName = 'host=127.0.0.1 port=5432 user=postgres password=password dbname=shop sslmode=disable',
+            },
+        },
+    },
 }
