f5ee87f667959b5464c431b45633f24f4bd408b4
Mehran <mehranhhm@gmail.com>
2023-03-20 10:41:32 +0330

    config update

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -33,8 +33,6 @@ nvim_tree.setup {
     hijack_netrw = true,
     hijack_unnamed_buffer_when_opening = false,
     ignore_buffer_on_setup = false,
-    open_on_setup = false,
-    open_on_setup_file = false,
     open_on_tab = false,
     sort_by = "name",
     update_cwd = true,
@@ -132,11 +130,6 @@ nvim_tree.setup {
         enable = true,
         update_cwd = true,
         ignore_list = {},
-    },
-    ignore_ft_on_setup = {
-        "startify",
-        "dashboard",
-        "alpha",
     },
     system_open = {
         cmd = "",
