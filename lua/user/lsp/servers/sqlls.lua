d6e7019ef4d1364c5695a3622a502eb707feb5a2
MHN <mehranhn@protonmail.com>
2023-11-14 16:10:36 +0330

    config update

--- lua/user/lsp/servers/sqlls.lua
+++ lua/user/lsp/servers/sqlls.lua
@@ -1,5 +1,9 @@
+local util = require 'lspconfig.util'
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-require'lspconfig'.sqlls.setup{
+
+require'lspconfig'.sqlls.setup {
+    cmd = { 'sql-language-server', 'up', '--method', 'stdio' },
     on_attach = require("user.lsp.handlers").on_attach,
     capabilities = capabilities,
+    root_dir = util.root_pattern(".git", ".sqllsrc.json")
 }
