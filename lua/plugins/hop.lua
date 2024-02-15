d86ae3c99b5624d8d23aac22986deff461a4df2d
MHN <mehranhn@protonmail.com>
2024-02-15 16:54:36 +0330

    config update

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -8,8 +8,8 @@ return {
         vim.api.nvim_set_keymap('', 'f', "<cmd>lua require('hop').hint_char1()<cr>", {})
         -- vim.api.nvim_set_keymap('', 'T', "<cmd>lua require('hop').hint_lines()<cr>", {})
         -- vim.api.nvim_set_keymap('', 'f', "<cmd>lua require('hop').hint_words({ inclusive_jump = true })<cr>", {})
-        vim.api.nvim_set_keymap('', 'T', "<cmd>lua require('hop').hint_words()<cr>", {})
-        vim.api.nvim_set_keymap('', 's', "<cmd>lua require('hop').hint_words({ current_line_only = true })<cr>", {})
+        --[[ vim.api.nvim_set_keymap('', 'T', "<cmd>lua require('hop').hint_words()<cr>", {}) ]]
+        --[[ vim.api.nvim_set_keymap('', 's', "<cmd>lua require('hop').hint_words({ current_line_only = true })<cr>", {}) ]]
         -- vim.api.nvim_set_keymap('', 's', "<cmd>lua require('hop').hint_char1({ current_line_only = true })<cr>", {})
         -- you can configure Hop the way you like here; see :h hop-config
         require("hop").setup(opts)
