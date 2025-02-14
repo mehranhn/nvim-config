c09473f7a39bb2ce168a0d082007f312e530ada3
MHN <mehranhn@protonmail.com>
2025-02-14 22:47:04 +0330

    config update

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -5,7 +5,18 @@ return {
             pcall(require("nvim-treesitter.install").update { with_sync = true })
         end,
         config = function()
-            require("ts_context_commentstring").setup()
+            require("ts_context_commentstring").setup({
+                config = {
+                    javascript = {
+                        __default = '// %s',
+                        jsx_element = '{/* %s */}',
+                        jsx_fragment = '{/* %s */}',
+                        jsx_attribute = '// %s',
+                        comment = '// %s',
+                    },
+                    typescript = { __default = '// %s', __multiline = '/* %s */' },
+                }
+            })
             vim.g.skip_ts_context_commentstring_module = true;
             require("nvim-treesitter.configs").setup({
                 modules = {},
@@ -48,9 +59,6 @@ return {
                 },
             })
         end,
-    },
-    {
-        "JoosepAlviste/nvim-ts-context-commentstring",
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
