4c302da122ddbf41c316d86460770ec9309d0c6f
Mehran Hadinejad <mehranhn@protonmail.com>
2025-08-22 13:44:58 +0330

    config update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -18,11 +18,8 @@ return {
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = formatters_by_ft,
-        lua = { "stylua" },
-        rust = { "rustfmt" },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
         default_format_opts = {
-            lsp_format = "fallback",
+            lsp_format = "last",
         },
     }
 }
