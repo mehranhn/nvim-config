c1df0d5cf9d7eb7ee4396768d6ed673880d4dd05
Mehran Hadinejad <mehranhn@protonmail.com>
2025-10-14 16:00:58 +0330

    disable plugin update notify

--- init.lua
+++ init.lua
@@ -22,5 +22,5 @@ require("lazy").setup({
         { import = "plugins" },
     },
     install = { colorscheme = { "vscode" } },
-    checker = { enabled = true },
+    checker = { enabled = true, notify = false },
 });
