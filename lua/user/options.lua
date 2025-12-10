75737aaff65e8d1c995eae45662452e630c32c19
Mehran Hadinejad <mehranhn@protonmail.com>
2025-12-10 13:08:04 +0330

    update options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,10 @@
 vim.opt.backup = false                                      -- creates a backup file
 vim.opt.clipboard = "unnamedplus"                           -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 2                                       -- more space in the neovim command line for displaying messages
-vim.opt.completeopt = { "menuone", "noinsert", "noselect" } -- mostly just for cmp
+-- vim.opt.completeopt = { "menuone", "noinsert", "noselect" } -- mostly just for cmp
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.keymap.set("i", "<C-n>", "<Nop>")
+vim.keymap.set("i", "<C-p>", "<Nop>")
 vim.opt.conceallevel = 0                                    -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8"                              -- the encoding written to a file
 vim.opt.hlsearch = true                                     -- highlight all matches on previous search pattern
