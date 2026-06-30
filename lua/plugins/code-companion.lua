7952847d524a0105777a235e779fe4e08399b7bc
Mehran Hadinejad <mehranhn@protonmail.com>
2026-06-30 04:48:57 +0330

    added code companion

---
+++ lua/plugins/code-companion.lua
@@ -0,0 +1,12 @@
+return {
+    "olimorris/codecompanion.nvim",
+    enabled = false,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        opts = {
+            log_level = "DEBUG", -- or "TRACE"
+        },
+    },
+}
