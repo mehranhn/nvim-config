dda2cac4c1449140701a2767578d95024ea53662
Mehran Hadinejad <mehranhn@protonmail.com>
2026-07-01 00:42:00 +0330

    updated keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,19 +1,7 @@
 local opts = { noremap = true, silent = true }
 
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
-
-local function nxomap(lhs, rhs, o)
-    vim.api.nvim_set_keymap("n", lhs, rhs, o)
-    vim.api.nvim_set_keymap("x", lhs, rhs, o)
-    vim.api.nvim_set_keymap("o", lhs, rhs, o)
-end
-local function nxmap(lhs, rhs, o)
-    vim.api.nvim_set_keymap("n", lhs, rhs, o)
-    vim.api.nvim_set_keymap("x", lhs, rhs, o)
-end
 -- Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+vim.keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -27,102 +15,96 @@ vim.g.maplocalleader = " "
 
 -- Colemak
 -- general
-nxomap("k", "i", { noremap = true })
-nxomap("K", "I", { noremap = true })
-nxomap("i", "<NOP>", { noremap = true })
-nxomap("I", "<NOP>", { noremap = true })
-nxomap("l", "<NOP>", { noremap = true })
-nxomap("L", "<NOP>", { noremap = true })
-nxomap("j", "<NOP>", { noremap = true })
-nxomap("J", "<NOP>", { noremap = true })
-keymap("n", "U", "<CMD>redo<CR>", opts)
+vim.keymap.set({ "n", "x", "o" }, "k", "i", { noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "K", "I", { noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "i", "<NOP>", { noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "I", "<NOP>", { noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "l", "<NOP>", { noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "L", "<NOP>", { noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "j", "<NOP>", { noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "J", "<NOP>", { noremap = true })
+vim.keymap.set("n", "U", "<CMD>redo<CR>", opts)
 -- keymap("n", "<C-r>", "<Nop>", opts)
-keymap("n", ",", "n", opts)
-keymap("n", ".", "N", opts)
+vim.keymap.set("n", ",", "n", opts)
+vim.keymap.set("n", ".", "N", opts)
 
 -- Delete all keybindings starting with 'i' in Visual mode
 local mappings = vim.api.nvim_get_keymap("v")
 for _, mapping in ipairs(mappings) do
     if mapping.lhs:sub(1, 1) == "i" then
-        pcall(vim.api.nvim_del_keymap, "v", mapping.lhs)
-        pcall(vim.api.nvim_del_keymap, "x", mapping.lhs)
-        pcall(vim.api.nvim_del_keymap, "s", mapping.lhs)
+        pcall(vim.keymap.del, { "v", "x", "s" }, mapping.lhs)
     end
 end
 
 -- navigation
 -- nxomap("h", "h", opts)
-nxomap("n", "j", opts)
-nxomap("e", "k", opts)
-nxomap("i", "l", opts)
+vim.keymap.set({ "n", "x", "o" }, "n", "j", opts)
+vim.keymap.set({ "n", "x", "o" }, "e", "k", opts)
+vim.keymap.set({ "n", "x", "o" }, "i", "l", opts)
 
-nxomap("H", "^", opts)
-nxomap("N", "5<DOWN>", opts)
-nxomap("E", "5<UP>", opts)
-nxomap("I", "<END>", opts)
+vim.keymap.set({ "n", "x", "o" }, "H", "^", opts)
+vim.keymap.set({ "n", "x", "o" }, "N", "5<DOWN>", opts)
+vim.keymap.set({ "n", "x", "o" }, "E", "5<UP>", opts)
+vim.keymap.set({ "n", "x", "o" }, "I", "<END>", opts)
 
--- nxmap("<C-L>", "<C-d>", opts)
--- nxmap("<C-U>", "<C-u>", opts)
+-- vim.keymap.set({"n", "x"}, "<C-L>", "<C-d>", opts)
+-- vim.keymap.set({"n", "x"}, "<C-U>", "<C-u>", opts)
 
--- keymap("n", "<C-E>", "<C-y>", opts)
--- keymap("n", "<C-N>", "<C-e>", opts)
+-- vim.keymap.set("n", "<C-E>", "<C-y>", opts)
+-- vim.keymap.set("n", "<C-N>", "<C-e>", opts)
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<A-h>", "<C-w>h", opts)
-keymap("n", "<A-n>", "<C-w>j", opts)
-keymap("n", "<A-e>", "<C-w>k", opts)
-keymap("n", "<A-i>", "<C-w>l", opts)
+vim.keymap.set("n", "<A-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<A-n>", "<C-w>j", opts)
+vim.keymap.set("n", "<A-e>", "<C-w>k", opts)
+vim.keymap.set("n", "<A-i>", "<C-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
--- keymap("n", "<S-i>", ":bnext<CR>", opts)
--- keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "<Tab>", "<CMD>bnext<CR>", opts)
-keymap("n", "<S-Tab>", "<CMD>bprevious<CR>", opts)
+-- vim.keymap.set("n", "<S-i>", ":bnext<CR>", opts)
+-- vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<Tab>", "<CMD>bnext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", "<CMD>bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<C-n>", ":m .+1<CR>", opts)
-keymap("n", "<C-e>", ":m .-2<CR>", opts)
+vim.keymap.set("n", "<C-n>", ":m .+1<CR>", opts)
+vim.keymap.set("n", "<C-e>", ":m .-2<CR>", opts)
 
 -- Press ne fast to enter
 -- Insert --
--- keymap("i", "ne", "<ESC>", opts)
--- keymap("l", "<C-h>", "<left>", opts)
--- keymap("l", "<C-n>", "<down>", opts)
--- keymap("l", "<C-e>", "<up>", opts)
--- keymap("l", "<C-i>", "<right>", opts)
+-- vim.keymap.set("i", "ne", "<ESC>", opts)
+-- vim.keymap.set("l", "<C-h>", "<left>", opts)
+-- vim.keymap.set("l", "<C-n>", "<down>", opts)
+-- vim.keymap.set("l", "<C-e>", "<up>", opts)
+-- vim.keymap.set("l", "<C-i>", "<right>", opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "<C-n>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<C-e>", ":move '<-2<CR>gv-gv", opts)
--- keymap("x", "<", "<gv", opts)
--- keymap("x", ">", ">gv", opts)
-keymap("x", "<Tab>", ">gv", opts)
-keymap("x", "<S-Tab>", "<gv", opts)
+vim.keymap.set("x", "<C-n>", ":move '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "<C-e>", ":move '<-2<CR>gv-gv", opts)
+-- vim.keymap.set("x", "<", "<gv", opts)
+-- vim.keymap.set("x", ">", ">gv", opts)
+vim.keymap.set("x", "<Tab>", ">gv", opts)
+vim.keymap.set("x", "<S-Tab>", "<gv", opts)
 
 -- clipboard
-nxmap("<C-p>", '"ap', opts)
-nxmap("<C-P>", '"aP', opts)
-nxmap("d", '"ad', opts)
-nxmap("D", '"aD', opts)
-nxmap("c", '"ac', opts)
-nxmap("C", '"aC', opts)
-nxmap("x", '"ax', opts)
-nxmap("X", '"aX', opts)
+vim.keymap.set({ "n", "x" }, "<C-p>", '"ap', opts)
+vim.keymap.set({ "n", "x" }, "<C-P>", '"aP', opts)
+vim.keymap.set({ "n", "x" }, "d", '"ad', opts)
+vim.keymap.set({ "n", "x" }, "D", '"aD', opts)
+vim.keymap.set({ "n", "x" }, "c", '"ac', opts)
+vim.keymap.set({ "n", "x" }, "C", '"aC', opts)
+vim.keymap.set({ "n", "x" }, "x", '"ax', opts)
+vim.keymap.set({ "n", "x" }, "X", '"aX', opts)
 
-keymap("c", "<C-p>", '<C-r>"', opts)
--- keymap("c", "<C-P>", "\"+P", opts)
+vim.keymap.set("c", "<C-p>", '<C-r>"', opts)
+-- vim.keymap.set("c", "<C-P>", "\"+P", opts)
 
 -- macro
-keymap("", "Q", "@q", opts)
-keymap("", "F", "@f", opts)
-keymap("", "#", "@w", opts)
-keymap("", "$", "@p", opts)
-keymap("", "^", "@g", opts)
+vim.keymap.set("", "Q", "@q", opts)
